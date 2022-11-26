43edd49631b89550d0fd22c2b897fe75db9e697b
matheushpr9 <113607124+matheushpr9@users.noreply.github.com>
2022-11-26 07:26:27 -0300

    Adicionado a função tratadados, que trata a base de dados de fornecedores

---
+++ tratarDados.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def tratar_dados(arquivo):
+    dados = pd.read_csv(arquivo, sep=';', encoding='utf8')
+    dados['Razão Social'] = dados['Razão Social'].apply(lambda x : x.strip("'"))
+    dados['Razão Social'] = dados['Razão Social'].apply(lambda x : "0"*(14-len(x)) + str(x) )
+    dados.columns = ['CNPJ', 'Razão Social', 'Contato']
+    dados['Contato'] = dados['Contato'].apply(lambda x : x.replace("\t",""))
+    return(dados)
+
+arquivo = 'projeto_lean/CNPJ_fornecedores.csv'
+dados = tratar_dados(arquivo)
+print(dados)
